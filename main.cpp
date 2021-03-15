924d573bfa59a88a3cc7108e57c920f842d00811
Nicolas Aubert <nicolas.aubert@he-arc.ch>
2021-03-15 22:35:32 +0100

    Add Hero class copy constructor. Add main.cpp with Hero class tests.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+* Auteur      : Aubert Nicolas
+* Date        : 15.03.2021
+* Contexte    : 
+* Description : 
+*/
+#include <iostream>
+#include "Hero.h"
+using namespace std;
+using namespace He_Arc::RPG;
+
+int main(int argc, char const *argv[])
+{
+    Hero Bobby = Hero(4, 5, 11, 20, "Bobby");
+    Hero Mamie = Hero(6, 3, 7, 50, "Mamie");
+    Bobby.Show();
+    Mamie.Show();
+    Bobby.Interact(Mamie);
+    return 0;
+}
