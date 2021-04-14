9caf3a91f4ab9fda2033f119d0d798596124f8e9
Nicolas Aubert <nicolas.aubert@he-arc.ch>
2021-04-14 13:36:10 +0200

    Adding Dagger class (small sword) with constant damage

---
+++ Dagger.cpp
@@ -0,0 +1,15 @@
+#include "Dagger.h"
+#include "Hero.h"
+using namespace std;
+namespace He_Arc::RPG
+{
+    // Constructors
+    Dagger::Dagger(){
+        this->Damage = 1;
+    }
+    // Methods
+    void Dagger::Stats(){
+        cout << "--- Dagger ---" << endl;
+        cout << "Damage : 1" << endl;
+    }
+}
---
+++ Dagger.h
@@ -0,0 +1,19 @@
+#ifndef DAGGER_H
+#define DAGGER_H
+#include <iostream>
+#include "Weapon.h"
+namespace He_Arc::RPG
+{
+    class Dagger : public Weapon
+    {
+        private:
+            // Attributs
+            int Damage = 1;
+        public:
+            // Constructors
+            Dagger();
+            // Methods
+            void Stats() override;
+    };
+}
+#endif
